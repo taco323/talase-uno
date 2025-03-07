bf536a0cbf8444b735db0a3a8416e261772be50f
taco323 <guillermobautista721@gmail.com>
2025-03-06 20:51:46 -0600

    cambio 1

---
+++ handpose/sketch.js
@@ -0,0 +1,37 @@
+let handPose;
+let video;
+let hands = [];
+
+function preload() {
+  handPose = ml5.handPose();
+}
+
+function setup() {
+  createCanvas(640, 480);
+    // Create the video and hide it
+    video = createCapture(VIDEO);
+    video.size(640, 480);
+    video.hide();
+    // Start detecting hands from the webcam video
+    handPose.detectStart(video, gotHands);
+  }
+
+// Callback function for when handPose outputs data
+function gotHands(results) {
+  // Save the output to the hands variable
+  hands = results;
+}
+  
+function draw() {
+  image(video, 0, 0, width, height);
+    // Draw all the tracked hand points
+    for (let i = 0; i < hands.length; i++) {
+      let hand = hands[i];
+      for (let j = 0; j < hand.keypoints.length; j++) {
+        let keypoint = hand.keypoints[j];
+        fill(0, 255, 0);
+        noStroke();
+        circle(keypoint.x, keypoint.y, 10);
+      }
+    }
+  }
